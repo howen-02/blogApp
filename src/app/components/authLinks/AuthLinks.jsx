7f78804ab63807e7fda501ecb2e299d4bdb88d02
Howenyap <e0985925@u.nus.edu>
2023-10-24 02:05:40 +0800

    Hamburger Menu

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 
 export const AuthLinks = () => {
+    const [open, setOpen] = useState(false);
     // temporary
-    const status = "notauthenticated"
-    return <>
-    {status==="notauthenticated" ? (
-        <Link href="/login">Login</Link>
-    ) : (
+    const status = "notauthenticated";
+    return (
         <>
-        <Link href="/write">Write</Link>
-        <span className={styles.link}>Logout</span>
+            {status === "notauthenticated" ? (
+                <Link href="/login" className={styles.link}>Login</Link>
+            ) : (
+                <>
+                    <Link href="/write" className={styles.link}>Write</Link>
+                    <span className={styles.link}>Logout</span>
+                </>
+            )}
+            <div className={styles.burger} onClick={()=>setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+                    {status === "notauthenticated" ? (
+                        <Link href="/login">Login</Link>
+                    ) : (
+                        <>
+                            <Link href="/write">Write</Link>
+                            <span className={styles.link}>Logout</span>
+                        </>
+                    )}
+                </div>
+            )}
         </>
-    )}
-    </>
+    );
 };
 
 export default AuthLinks;
